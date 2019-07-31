b055d18aca20aec7b972a40c690551bc7f23e6cb
Alan Raff <alanr917@hotmail.com>
2019-07-31 19:13:35 -0400

    installed and configured dependencies for timelapse creation

--- install-deps.sh
+++ install-deps.sh
@@ -1,9 +1,17 @@
 apt-get update
 apt-get install -y apache2
 apt-get install -y php libapache2-mod-php
+apt-get install -y ffmpeg php-common gcc
+apt-get install -y imagemagick
+apt-get install -y php-imagemagick
 apt-get install -y mysql-server php-mysql
 apt-get install -y python-mysqldb
 
 mkdir /home/pi/SHyPi
 chown www-data:www-data /home/pi/SHyPi/
 chmod 777 /home/pi/SHyPi/
+
+chown www-data:www-data /var/www/html/timelapses/
+chmod 777 /var/www/html/timelapses/
+
+systemctl restart apache2
