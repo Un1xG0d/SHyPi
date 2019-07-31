9a28a3a2377db8ab53cf974217dd2a63b44dd102
Alan Raff <alanr917@hotmail.com>
2019-07-31 19:44:20 -0400

    added redirect to new timelapse URL after creation

--- html/processtimelapse.php
+++ html/processtimelapse.php
@@ -4,10 +4,11 @@ $month = htmlspecialchars($_POST['month']);
 $day = htmlspecialchars($_POST['day']);
 $filename = htmlspecialchars($_POST['filename']);
 
-$mkvidcmd = "ffmpeg -r 24 -pattern_type glob -i 'camimages/".$year.$month.$day."*.jpg' -vcodec libx264 -pix_fmt yuv420p /home/pi/SHyPi/".$filename.".mp4";
+$mkvidcmd = "ffmpeg -r 24 -pattern_type glob -i 'camimages/".$year.$month.$day."*.jpg' -vcodec libx264 -pix_fmt yuv420p /var/www/html/timelapses/".$filename.".mp4";
 system($mkvidcmd);
 
-$mvvidcmd = "cp /home/pi/SHyPi/".$filename." /var/www/html/timelapses/";
-system($mvvidcmd);
+$url = "Location: http://10.0.0.153/timelapses/".$filename.".mp4";
+header($url);
+die();
 
 ?>
